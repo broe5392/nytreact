3ca518f3234619b2a548864084695b96d5780261
Brandon B <broe5392@yahoo.com>
2018-10-06 22:16:59 -0400

    ran the build script

---
+++ client/build/precache-manifest.c6e36f0335fb083154a6e0e80731f346.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "23e69cc1cd66f4d4e281",
+    "url": "/static/js/main.23e69cc1.chunk.js"
+  },
+  {
+    "revision": "4aa8e932caae7f805f7c",
+    "url": "/static/js/1.4aa8e932.chunk.js"
+  },
+  {
+    "revision": "23e69cc1cd66f4d4e281",
+    "url": "/static/css/main.aa6bc9ed.chunk.css"
+  },
+  {
+    "revision": "ba1c45f5e7af49cf8ff398543ab4d600",
+    "url": "/index.html"
+  }
+];
